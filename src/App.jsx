040a44ee5a68732aca48a3d9b32ec4b35ee47803
Gogo602 <gogomarosi61@gmail.com>
2025-03-18 04:03:15 +0300

    installed react/tailwind/shadcn-ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen text-blue-500 text-3xl font-bold'>
+      My App
+    </div>
+  );
+};
+
+export default App;
