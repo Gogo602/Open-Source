0abba024c12fe52df1887d46f46a31c410c61960
Gogo602 <gogomarosi61@gmail.com>
2025-03-20 02:46:26 +0300

    working on home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center h-screen text-blue-500 text-3xl font-bold'>
-      Welcome
+    <div className='flex items-center justify-center h-screen'>
+      <div className='flex gap-30 px-40'>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure,
+          excepturi ipsa tempora veniam odit sint nemo culpa quae ipsum sed
+          iusto dicta fuga cumque quod repellat quo ducimus quisquam assumenda.
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius tempora
+          nesciunt impedit officia quibusdam, iste nam magni delectus voluptate
+          dolor vel provident quasi ipsam voluptatem, rerum temporibus!
+          Recusandae, obcaecati dolores!
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
